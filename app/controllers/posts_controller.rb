71c0ba0f1c96a5476db1fc61288c2fa5d87db90b
dannyb21892 <dannyb21892@verizon.net>
2018-03-19 11:03:42 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,14 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(params[:post])
+    redirect_to post_path(@post)
+  end
+
 end
